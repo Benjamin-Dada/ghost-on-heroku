0ce98047c9780e606390c7e99b09aab2b22dadfa
Benjamin-Dada <Benjamin.o.dada@gmail.com>
2018-08-25 16:21:11 +0100

    upgrade to Ghost 2 and Casper 2.5.1

---
+++ MigratorConfig.js
@@ -0,0 +1,26 @@
+module.exports = {
+    database: {
+        client: 'mysql',
+        connection: {
+            host: '127.0.0.1',
+            user: 'user',
+            password: 'password',
+            charset: 'utf8',
+            database: 'ghost'
+        }
+    },
+    migrationPath: process.cwd() + '/current/server/data/migrations',
+    currentVersion: 'your-current-database/project-version',
+    subfolder: 'upgrades'  [default: versions]
+}
+
+module.exports = {
+    database: {
+        client: 'sqlite3',
+        connection: {
+            filename: 'path/to/your.db'
+        }
+    },
+    migrationPath: process.cwd() + '/current/server/data/migrations',
+    currentVersion: 'your-current-database-version'
+}
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var ghost = require('ghost');
-var cluster = require('cluster');
+let ghost = require('ghost');
+let cluster = require('cluster');
 
 // Heroku sets `WEB_CONCURRENCY` to the number of available processor cores.
-var WORKERS = process.env.WEB_CONCURRENCY || 1;
+let WORKERS = process.env.WEB_CONCURRENCY || 1;
 
 if (cluster.isMaster) {
   // Master starts all workers and restarts them when they exit.
@@ -10,12 +10,12 @@ if (cluster.isMaster) {
     console.log(`Starting a new worker because PID: ${worker.process.pid} exited code ${code} from ${signal} signal.`);
     cluster.fork();
   });
-  for (var i = 0; i < WORKERS; i++) {
+  for (let i = 0; i < WORKERS; i++) {
     cluster.fork();
   }
 } else {
   // Run Ghost in each worker / processor core.
-  ghost().then(function (ghostServer) {
+  ghost().then((ghostServer) => {
     ghostServer.start();
   });
 }
